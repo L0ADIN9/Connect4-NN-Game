e4e8507b6a13f4dc3e60a6d4b1275cf5e7f3199c
Max DeWeese <maximiliandweese@gmail.com>
2026-04-28 15:41:32 -0700

    got the grid setup

--- src/main/java/org/example/BoardPanel.java
+++ src/main/java/org/example/BoardPanel.java
@@ -3,16 +3,46 @@ import javax.swing.*;
 import java.awt.*;
 
 public class BoardPanel extends JPanel{
+    private int padding = 20;
+    private int gap = 10;
     public BoardPanel() {
-        setBackground(Color.GRAY);
+        setBackground(Color.gray);
     }
-/*
+
+    // use repaint( ) to update board
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        g.setColor(Color.WHITE);
-        g.fillOval(50, 50, 60, 60);
-    }*/
+        int bHeight = getHeight() - padding *2;
+        int bWidth = (int)(bHeight*7/6);
+        if (bWidth > getWidth() - padding * 2) {
+            bWidth = getWidth() - padding * 2;
+            bHeight = (int)(bWidth * (6.0 / 7.0));
+        }
+        int cellScale = bWidth/ 7;
+
+        int centX = (getWidth()-bWidth)/2;
+        int centY = (getHeight()-bHeight)/2;
+
+        g.setColor(Color.BLUE);
+        g.fillRect(centX,centY ,bWidth,bHeight);
+        int[][] brd = GameHandler.getBoard();
+
+
+        for(int i = 0; i< 7; i++){
+            for(int j = 0; j< 6; j++){
+                Color cl = Color.white;
+                if(brd[i][j] == 1) cl = Color.red;
+                if(brd[i][j] == 2) cl = Color.yellow;
+
+                g.setColor(cl);
+                g.fillOval(centX + i*cellScale+(int)(.1*cellScale),centY+j*cellScale+(int)(.1*cellScale), (int)(.8*cellScale),(int)(.8*cellScale));
+
+
+            }
+        }
+
+    }
 
 }
--- src/main/java/org/example/GameWindow.java
+++ src/main/java/org/example/GameWindow.java
@@ -1,14 +1,33 @@
 package org.example;
 import javax.swing.*;
+import java.awt.*;
 
 public class GameWindow extends JFrame {
 
     public GameWindow() {
+        setBackground(Color.gray);
         setTitle("Connect 4");
         setSize(1000, 800);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+        BoardPanel board = new BoardPanel();
+        board.setSize(400,600);
 
-        add(new BoardPanel()); // UI lives here
+        JPanel rightSide = new JPanel();
+        rightSide.add(new JLabel("Turn: Player"));
+
+        JPanel leftSide = new JPanel();
+        leftSide.add(new JLabel("Turn: Player"));
+
+
+        rightSide.setBackground(Color.gray);
+        leftSide.setBackground(Color.gray);
+
+        add(board,BorderLayout.CENTER);
+        add(leftSide, BorderLayout.WEST);
+        add(rightSide, BorderLayout.EAST);
+
+
 
         setVisible(true);
     }
