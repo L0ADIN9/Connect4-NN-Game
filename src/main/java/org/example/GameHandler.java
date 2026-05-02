243f08d0f5ee010d085efa01fea7374746d02450
Max DeWeese <maximiliandweese@gmail.com>
2026-05-02 00:30:48 -0700

    feat: fixed win condition logic

--- src/main/java/org/example/GameHandler.java
+++ src/main/java/org/example/GameHandler.java
@@ -15,27 +15,40 @@ public class GameHandler {
         turnCnt = 0;
         while (turnCnt < 42){
             int mv=0;
+            int tp = -1;
             if (playerTurn) {
                 while (true){
                     mv= Main.getHumanInputPos();
                     if(isValidMove(mv)){
-                        move(mv, 1);
+                        tp = move(mv, 1);
                         break;
                     }
                 }
             }
+
             if (!playerTurn) {
+
                 Thread.sleep(500);
-                mv = Main.getBotInputPos();
-                move(mv, 2);
+
+                 mv = Main.getBotInputPos();
+               tp =  move(mv, 2);
+
             }
+
+            if(checkWin(mv,tp)){
+                GameWindow.boardP.repaint();
+                if(playerTurn) win(1);
+                else win(2);
+                return;
+            }
+
 
             playerTurn  = !playerTurn;
             turnCnt++;
             GameWindow.boardP.repaint();
 
         }
-        if(turnCnt>=42) draw();
+        draw();
 
 
     }
@@ -49,7 +62,7 @@ public class GameHandler {
     public static int[][] getBoard(){
         return board;
     }
-    public static void move(int pos, int who){
+    public static int move(int pos, int who){
         // pos is the x position of the dropped chip this move
         // who is which player's turn it is repersented numerically
         // 1 = human
@@ -59,10 +72,11 @@ public class GameHandler {
             top--;
             if(top <0){
                 System.out.println("ILLEGAL MOVE");
-                return;
+                return -1;
             }
         }
         board[top][pos] = who;
+        return top;
     }
     public static boolean isValidMove(int pos){
         if (0>pos || pos>7) return false;
@@ -81,6 +95,7 @@ public class GameHandler {
         int who = board[y][x];
         for(int xv= -1; xv<2; xv++){
             for(int yv = -1; yv<2; yv++){
+                //checks for edge of 4 row
                 for(int i = 0; i< 4; i++){
                     //checks to see if the vectors are both zerod
                     if(xv==0 &&yv==0)break;
@@ -91,6 +106,19 @@ public class GameHandler {
                     //checks if the val doesn't match the pattern
                     if(board[y+yv*i][x+xv*i]!= who)break;
                     else if (i==3) return true;
+                    // if these match for the 4th time in a row, then winner!
+                }
+                //checks for inside of 4 row
+                for(int i = -1; i< 3; i++){
+                    //checks to see if the vectors are both zerod
+                    if(xv==0 &&yv==0)break;
+                    //checks if the x val is out of bounds
+                    if(x+xv*i<0 ||x+xv*i>= 7)break;
+                    //checks if the y val is out of bounds
+                    if(y+yv*i<0 ||y+yv*i>= 6)break;
+                    //checks if the val doesn't match the pattern
+                    if(board[y+yv*i][x+xv*i]!= who)break;
+                    else if (i==2) return true;
                     // if these match for the 4th time in a row, then winner!
                 }
             }
